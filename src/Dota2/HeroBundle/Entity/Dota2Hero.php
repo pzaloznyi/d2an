c2815b952faea301a56b2a84641d122e307b91ab
Pavel Zaloznyi <pzaloznyi@gmail.com>
2013-12-02 18:21:28 +0200

    added hero entity

---
+++ src/Dota2/HeroBundle/Entity/Dota2Hero.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Dota2\HeroBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Dota2Hero
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Dota2\HeroBundle\Entity\Dota2HeroRepository")
+ */
+class Dota2Hero
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=1000)
+     */
+    private $description;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Dota2Hero
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Dota2Hero
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
